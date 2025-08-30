d630687877ed4986a82030ee2ab01f2abc0be071
LanRhyme <xiao_ren233@foxmail.com>
2025-08-31 00:51:02 +0800

    :tada: 新ui-背景效果

--- Natives/LauncherSplitViewController.m
+++ Natives/LauncherSplitViewController.m
@@ -12,6 +12,31 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
+    // Use the system background color which adapts to light/dark mode
+    self.view.backgroundColor = [UIColor systemBackgroundColor];
+
+    // Create colored circles that will be blurred by the view on top.
+    CGFloat circleSize = 400.0;
+    UIView *topLeftCircle = [[UIView alloc] initWithFrame:CGRectMake(-circleSize/2, -circleSize/2, circleSize, circleSize)];
+    topLeftCircle.backgroundColor = [UIColor.greenColor colorWithAlphaComponent:0.5];
+    topLeftCircle.layer.cornerRadius = circleSize / 2.0;
+
+    UIView *bottomRightCircle = [[UIView alloc] initWithFrame:CGRectMake(self.view.bounds.size.width - circleSize/2, self.view.bounds.size.height - circleSize/2, circleSize, circleSize)];
+    bottomRightCircle.backgroundColor = [UIColor.greenColor colorWithAlphaComponent:0.5];
+    bottomRightCircle.layer.cornerRadius = circleSize / 2.0;
+    bottomRightCircle.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleTopMargin;
+
+    [self.view insertSubview:topLeftCircle atIndex:0];
+    [self.view insertSubview:bottomRightCircle atIndex:1];
+
+    // Create a blur view to place over the circles, acting as a frosted glass effect for the entire background.
+    UIBlurEffect *blurEffect = [UIBlurEffect effectWithStyle:UIBlurEffectStyleSystemMaterial];
+    UIVisualEffectView *blurEffectView = [[UIVisualEffectView alloc] initWithEffect:blurEffect];
+    blurEffectView.frame = self.view.bounds;
+    blurEffectView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+    blurEffectView.alpha = 0.5; // As per user request for 50% transparency
+    [self.view insertSubview:blurEffectView atIndex:2];
+
     self.preferredDisplayMode = UISplitViewControllerDisplayModeOneBesideSecondary;
 
     if (@available(iOS 14.0, *)) {
